50450449e8def675af33041938b187c1595f030e
Alexander Bulgakov <anbulgakov@gmail.com>
2023-04-08 00:09:45 +0300

    added constants and router modules

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const CURRENCIES_LIST = ["USD", "EUR", "GBP", "JPY"];
+const CurrenciesRatesUrl = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+export { CURRENCIES_LIST, CurrenciesRatesUrl };
---
+++ src/data.js
@@ -0,0 +1,18 @@
+import { CURRENCIES_LIST, CurrenciesRatesUrl } from "./constants.js";
+
+// Загрузить курсы валют
+async function fetchCurrencies() {
+  const responce = await fetch(CurrenciesRatesUrl);
+  const json = await responce.json();
+  return json.Valute;
+}
+
+// Вернуть объект с нужными курсами для вывода их в интерфейс
+async function getData() {
+  const rates = {};
+  let currencies = await fetchCurrencies();
+  CURRENCIES_LIST.forEach(item => rates[item] = currencies[item]?.Value);
+  return rates;
+}
+
+export default getData;
