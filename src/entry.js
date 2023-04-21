7ef7672cf8da3eef7f07c5872b82c6686814ce84
Alexander Bulgakov <anbulgakov@gmail.com>
2023-04-21 23:07:28 +0300

    cleaned Card.js

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,7 @@
 import styles from "./styles/index.scss";
-import image from "./images/cat.png";
+import catImage from "./images/cat.png";
 import handleLocation from "./router.js";
-// import card from "./components/Card.html";
-// import { rotateArrow } from "./helpers";
 
-document.querySelector(".navbar__image img").src = image;
+document.querySelector(".navbar__image img").src = catImage;
 
 handleLocation();
-
