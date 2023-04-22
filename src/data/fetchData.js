4f89cbb7d4719ce71f2e68daeddaee8a87618486
Alexander Bulgakov <anbulgakov@gmail.com>
2023-04-22 19:53:36 +0300

    added fetching error handling

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -2,9 +2,13 @@ import { CURRENCIES_LIST, CURRENCIES_RATES_URL } from "../constants.js";
 
 // Загрузить курсы валют
 async function fetchCurrencies() {
-  const responce = await fetch(CURRENCIES_RATES_URL);
-  const json = await responce.json();
-  return json.Valute;
+  try {
+    const responce = await fetch(CURRENCIES_RATES_URL);
+    const json = await responce.json();
+    return json.Valute;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // Вернуть объект только с нужными курсами
